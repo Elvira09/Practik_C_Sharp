43382cd55a6f917756934b8ef7b568997d9264ac
Elvira09 <zag0909@gmail.com>
2022-02-20 14:55:07 +0300

    Написали код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+	int index = 0;
+	while (index < collection.Length)
+	{
+		collection[index] = new Random().Next(-50, 50);
+		index++;
+	}
+}
+
+void PrintArray(int[] collection)
+{
+	for (int i = 0; i < collection.Length; i++)
+	{
+		Console.Write($"{collection[i]} ");
+	}
+	Console.WriteLine();
+}
+
+int SearchItems(int[] collection)
+{
+	int count = 0;
+	for (int i = 0; i < collection.Length; i++)
+	{
+		if (collection[i] > 8)
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
+void FillArraySelectedItemsAndPrint(int[] collection)
+{
+	int[] newCollection = new int[SearchItems(collection)];
+	for (int i = 0; i < collection.Length; i++)
+	{
+		int j = SearchItems(collection) - 1;
+		
+			if (collection[i] > 8)
+			{
+				newCollection[j] = collection[i];
+				Console.Write($"{newCollection[j]} ");
+			}
+	}
+}
+
+Console.Clear();
+
+int[] array = new int[25];
+FillArray(array);
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+Console.WriteLine("Результирующий массив: ");
+FillArraySelectedItemsAndPrint(array);
+Console.WriteLine();
+
